71dbd0f5acc54756f71f6cd2cd03a98ce70c26ad
Yann Periney <periney1u@fst-ats-315-l09.fst.site.univ-lorraine.fr>
2021-04-01 11:38:26 +0200

    ajout de tests.c

---
+++ projet/tests.c
@@ -0,0 +1,20 @@
+/**
+ * \file tests.c
+ * \brief Fichier de test pour les fonction de game_event
+ * \author Periney Yann Victor Dallé
+ * \version 1
+ * \date 1 avril 2021
+*/
+
+#include "param.h"
+
+/**
+ *  \brief Programme principal qui implémente la boucle du jeu.
+ * \param argc Taille du tableau argv.
+ * \param argv Pointeur vers un tableau de char de taille argc.
+ * \return 0, si il n'y a pas eu d'erreurs.
+ */
+int main( int argc, char* argv[] ){
+
+    return 0;
+}
