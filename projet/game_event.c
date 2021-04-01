e73965c1ce7ec459050f94e5844aa42e5a978e47
Victor Dallé <dalle5u@etu.univ-lorraine.fr>
2021-04-01 11:14:38 +0200

    module game_event fini

---
+++ projet/game_event.c
@@ -0,0 +1,105 @@
+#include "game_event.h"
+#include "param.h"
+
+void init_sprite(sprite_t *sprite, int x, int y, int w, int h){
+	sprite->x=x;
+	sprite->y=y;
+	sprite->w=w;
+	sprite->h=h;
+}
+
+void print_sprite(sprite_t *sprite){
+	printf("Position en x = %d\nPosition en y = %d\nLargeur du sprite = %d\nHauteur du sprite = %d\n",sprite->x,sprite->y,sprite->w,sprite->h);
+}
+
+void init_data(world_t * world){
+    // On n'est pas à la fin du jeu
+    world->gameover = 0;
+
+	// Initialisation du vaisseau
+	init_sprite(&world->vaisseau,SCREEN_WIDTH/2 - SHIP_SIZE/2,SCREEN_HEIGHT - SHIP_SIZE*2,SHIP_SIZE,SHIP_SIZE);
+
+    // Initialisation de la ligne d'arrivée
+    init_sprite(&world->arrival,0,FINISH_LINE_HEIGHT,SCREEN_WIDTH,FINISH_LINE_HEIGHT);
+
+    // Position initiale du sprite vaisseau
+    print_sprite(&world->vaisseau);
+    printf("=======================\n");
+
+    // Position initiale du sprite arrival
+    print_sprite(&world->arrival);
+    printf("=======================\n");
+
+    // Initialisation de la vy
+    world->vy = INITIAL_SPEED;
+
+    // Initialisation d'un mur de météorites.
+    init_sprite(&world->mur, SCREEN_WIDTH/2 - 3*METEORITE_SIZE/2, SCREEN_HEIGHT/2 - 7*METEORITE_SIZE/2, METEORITE_SIZE, METEORITE_SIZE);
+
+}
+
+void apply_sprite(SDL_Renderer *renderer, SDL_Texture *texture, sprite_t *sprite){
+	apply_texture(texture, renderer, sprite->x,sprite->y);
+}
+
+void clean_data(world_t *world){
+    /* Utile uniquement si vous avez fait de l'allocation dynamique (malloc); la fonction ici doit permettre de libérer la mémoire (free) */ 
+}
+
+int is_game_over(world_t *world){
+    return world->gameover;
+}
+
+void update_data(world_t *world){
+    world->arrival.y += world->vy;
+    world->mur.y += world->vy;
+
+}
+
+void handle_events(SDL_Event *event,world_t *world){
+    Uint8 *keystates;
+    while( SDL_PollEvent( event ) ) {
+        
+        //Si l'utilisateur a cliqué sur le X de la fenêtre
+        if(event->type == SDL_QUIT) {
+            //On indique la fin du jeu
+            world->gameover = 1;
+        }
+       
+        //si une touche est appuyée
+        if(event->type == SDL_KEYDOWN){
+            //si la touche appuyée est 'D'
+            if(event->key.keysym.sym == SDLK_d){
+                printf("La touche D est appuyée\n");
+                world->vaisseau.x+= MOVING_STEP;
+              }
+            
+            //si la touche appuyée est 'Q'
+            if(event->key.keysym.sym == SDLK_q){
+                printf("La touche Q est appuyée\n");
+                world->vaisseau.x-= MOVING_STEP;
+            }
+
+            if(event->key.keysym.sym == SDLK_ESCAPE){
+            //On indique la fin du jeu
+            printf("La touche ECHAP est appuyée\n");
+            world->gameover = 1;
+            }
+
+            if(event->key.keysym.sym == SDLK_z){
+            //On indique la fin du jeu
+            printf("La touche Z est appuyée\n");
+            world->vy=world->vy+1;
+            }
+
+            if(event->key.keysym.sym == SDLK_s){
+            //On indique la fin du jeu
+            printf("La touche S est appuyée\n");
+            world->vy=world->vy-1;
+            }
+
+            print_sprite(&world->vaisseau); // Affichage coordonnées à chaque déplacement
+            printf("=======================\n");
+        }
+    }
+}
---
+++ projet/game_event.h
@@ -0,0 +1,60 @@
+#ifndef DEF_GAME_EVENT_H
+#define DEF_GAME_EVENT_H
+
+/**
+ * \brief La fonction initialise les données d'un sprite selon les valeurs entrées.
+ * \param sprite Pointeur vers sprite_t pour l'initialisation des données.
+ * \param x Coordonnée x du sprite.
+ * \param y Coordonnée y du sprite.
+ * \param w Largeur du sprite.
+ * \param h Hauteur du sprite.
+ */
+void init_sprite(sprite_t *sprite, int x, int y, int w, int h);
+
+/**
+ * \brief La fonction affiche les information (coordonées) d'un sprite.
+ * \param sprite Variable de type sprite_t de laquelle on affiche les informations.
+ */
+void print_sprite(sprite_t *sprite);
+
+/**
+ * \brief La fonction initialise les données du monde du jeu.
+ * \param world Les données du monde.
+ */
+void init_data(world_t * world);
+
+/**
+ * \brief La fonction applique un sprite au renderer.
+ * \param renderer Renderer vers lequel on envoie les textures et les sprites.
+ * \param texture Texture envoyée vers le renderer.
+ * \param sprite Sprite envoyé vers le renderer.
+ */
+void apply_sprite(SDL_Renderer *renderer, SDL_Texture *texture, sprite_t *sprite);
+
+/**
+ * \brief La fonction nettoie les données du monde.
+ * \param world Les données du monde.
+ */
+void clean_data(world_t *world);
+
+/**
+ * \brief La fonction indique si le jeu est fini en fonction des données du monde.
+ * \param world Les données du monde.
+ * \return 1 si le jeu est fini, 0 sinon.
+ */
+int is_game_over(world_t *world);
+
+/**
+ * \brief La fonction met à jour les données en tenant compte de la physique du monde.
+ * \param world Les données du monde.
+ */
+void update_data(world_t *world);
+
+/**
+ * \brief La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités.
+ * \param event Paramètre qui contient les événements.
+ * \param world Les données du monde.
+ */
+void handle_events(SDL_Event *event,world_t *world);
+
+#endif
